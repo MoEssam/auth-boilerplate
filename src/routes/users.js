14111a80f668e66f64c47b727ff616dea1ba1231
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-06 19:04:28 +0200

    added user login

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,4 +13,16 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
+    res.send({ user });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
