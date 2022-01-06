c9366b80cba3dc41d90da70eb5ed7650c0a911c2
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-06 13:56:43 +0200

    hotfix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Example app listening at http://localhost:" + port);
 });
