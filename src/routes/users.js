6d2f31b1b1d8179faed55c5e31552f8bb4956d2c
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-06 19:38:43 +0200

    added JWT auth

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -19,10 +21,15 @@ router.post("/login", async (req, res) => {
       req.body.username,
       req.body.password
     );
-    res.send({ user });
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
+});
+
+router.get("/private", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 module.exports = router;
