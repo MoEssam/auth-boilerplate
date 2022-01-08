2f2983c564e58fd96bccb88f4af20418c1c36894
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-08 15:49:24 +0200

    added cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,15 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
