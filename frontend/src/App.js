67a9208033e4daa07c0673e1f77333da1b1a8c0d
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-13 00:05:21 +0200

    added navbar & dashboard hotfix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   const myStorage = window.localStorage;
@@ -18,8 +19,12 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Navbar myStorage={myStorage} setCurrentUser={setCurrentUser} />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={<Dashboard myStorage={myStorage} />}
+        />
         <Route
           path="/"
           element={
