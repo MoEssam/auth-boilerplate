e023f87f6527d06984f33931e1467584291120f2
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-13 00:36:20 +0200

    dashbaord route authentication

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,14 @@
 export default function Dashboard({ myStorage }) {
   const user = myStorage.getItem("user");
+  const token = myStorage.getItem("token");
   return (
     <div>
-      Authentication System Dashboard
-      <h1>Hello {user}</h1>
+      {token ? (
+        <h1>Hello {user}</h1>
+      ) : (
+        <p>You are not authorized, please log in</p>
+      )}
+      {/* Authentication System Dashboard */}
     </div>
   );
 }
