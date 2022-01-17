63a997dad1c6f656f5a638ee58b043a5aa9584cb
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-17 18:25:09 +0200

    hotfix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import * as React from "react";
-import "./app.css";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
@@ -10,12 +9,6 @@ import Navbar from "./components/Navbar";
 function App() {
   const myStorage = window.localStorage;
   const [currentUser, setCurrentUser] = useState(null);
-  const [showRegister, setShowRegister] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
-  const handleLogout = () => {
-    myStorage.removeItem("user");
-    setCurrentUser(null);
-  };
 
   return (
     <BrowserRouter>
@@ -25,38 +18,7 @@ function App() {
           path="/dashboard"
           element={<Dashboard myStorage={myStorage} />}
         />
-        <Route
-          path="/"
-          element={
-            <div>
-              {/* {currentUser ? (
-                <button className="button logout" onClick={handleLogout}>
-                  Log Out
-                </button>
-              ) : (
-                <div className="buttons">
-                  <button
-                    className="button login"
-                    onClick={() => setShowLogin(true)}
-                  >
-                    Login
-                  </button>
-                  <button
-                    className="button register"
-                    onClick={() => setShowRegister(true)}
-                  >
-                    Register
-                  </button>
-                </div>
-              )} */}
-              {/* {showRegister && <Register setShowRegister={setShowRegister} />}
-              {showLogin && (
-                <Login myStorage={myStorage} setCurrentUser={setCurrentUser} />
-              )} */}
-              This is the Homepage
-            </div>
-          }
-        />
+        <Route path="/" element={<div>This is the Homepage</div>} />
         <Route
           path="/login"
           element={
