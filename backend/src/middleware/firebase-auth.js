041dba09204c1d072deb5390cdf349408e35738b
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-19 05:17:57 +0200

    modify backend firebase auth

--- backend/src/middleware/firebase-auth.js
+++ backend/src/middleware/firebase-auth.js
@@ -1,17 +1,22 @@
 const admin = require("../utils/firebase-config");
 
 const authFirebase = async (req, res, next) => {
-  try {
-    const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = await admin.auth().verifyIdToken(token);
-    if (!decoded) {
-      throw new Error();
+  const token1 = req.header("Authorization");
+  if (!token1) {
+    return null;
+  } else {
+    try {
+      const token = req.header("Authorization").replace("Bearer ", "");
+      const decoded = await admin.auth().verifyIdToken(token);
+      if (!decoded) {
+        throw new Error();
+      }
+      req.user = decoded;
+      req.token = token;
+      next();
+    } catch (e) {
+      res.status(401).send({ error: "Please Authenticate" });
     }
-    req.user = decoded;
-    req.token = token;
-    next();
-  } catch (e) {
-    res.status(401).send({ error: "Please Authenticate" });
   }
 };
 
