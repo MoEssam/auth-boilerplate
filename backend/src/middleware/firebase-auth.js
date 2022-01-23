3380901ca1bccbfb8d83364bca717de9ae764914
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-23 06:38:36 +0200

    firebase-auth sever-side

---
+++ backend/src/middleware/firebase-auth.js
@@ -0,0 +1,19 @@
+const admin = require("../utils/firebase-config");
+
+const authFirebase = async (req, res, next) => {
+  const token1 = req.header("Authorization");
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = await admin.auth().verifyIdToken(token);
+    if (!decoded) {
+      throw new Error();
+    }
+    req.user = decoded;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please Authenticate" });
+  }
+};
+
+module.exports = authFirebase;
