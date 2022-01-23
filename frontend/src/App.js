d58b2823559378248c6d7c9fce297d5cbfb37d02
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-23 03:54:56 +0200

    userAuthContext & re-routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,48 @@
-import * as React from "react";
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from "./components/Profile";
 
 function App() {
-  const myStorage = window.localStorage;
-  const [currentUser, setCurrentUser] = useState(null);
-
   return (
-    <BrowserRouter>
-      <Navbar myStorage={myStorage} setCurrentUser={setCurrentUser} />
-      <Routes>
-        <Route
-          path="/dashboard"
-          element={<Dashboard myStorage={myStorage} />}
-        />
-        <Route path="/" element={<div>This is the Homepage</div>} />
-        <Route
-          path="/login"
-          element={
-            <Login myStorage={myStorage} setCurrentUser={setCurrentUser} />
-          }
-        />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <UserAuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<div>This is the Homepage</div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <p>
+                hehe no route error <b>404</b>
+              </p>
+            }
+          />
+        </Routes>
+      </UserAuthContextProvider>
+    </div>
   );
 }
 
